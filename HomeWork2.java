d3126e3375b59262b70a1504581320f7444c0bf8
Daria <frank.darya@yandex.ru>
2022-02-12 22:22:59 +0100

    update

--- HomeWork2.java
+++ HomeWork2.java
@@ -10,27 +10,31 @@ public class HomeWork2 {
         boolean result = isNumberPositive(5);
         System.out.println(result ? "Positive" : "negative");
         System.out.println(isNumberNegative(5));
-        numberOfRows();
+        numberOfRows(("Hello everyone!"), 5);
         boolean result1 = yearLeap(2008);
         System.out.println(result1 ? "Год високосный, а это значит в нем 366 дней!" : "Год не високосный");
 
     }
 
     static boolean sumAmountIsWithin(int a, int b) {
+
         return a + b >= 10 && a + b <= 20;
     }
 
     static boolean isNumberPositive(int a) {
+
         return a >= 0;
     }
 
     static boolean isNumberNegative(int s) {
+
         return s < 0;
     }
 
-    static void numberOfRows() {
-        for (int i = 0; i < 5; i++)
-            System.out.println("Hello everyone!");
+    static void numberOfRows(String word, int a) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(word);
+        }
     }
 
     static boolean yearLeap(int year) {
